991e484a627f3931af61e76ab2e7f9b8a31922f2
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-05-15 22:42:35 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,20 @@ module FormsLab
 
     # code other routes/actions here
 
+    get "/" do
+    	"Welcome to the Nested Forms Lab! \n let's navigate to the '/new'"
+    end
+
+    get "/new" do
+    	erb :"pirates/new"
+    end
+
+    post "/pirates" do
+        @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
+        @ships = params[:pirate][:ships].map do |ship|
+            Ship.new(ship[:name], ship[:type], ship[:booty])
+        end
+    	erb :"pirates/show"
+    end
   end
 end
